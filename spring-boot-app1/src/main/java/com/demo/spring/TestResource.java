f43604ae174b1e84c58f7396d07781281506fe56
user120 <user120@Microservices34.alchemycloud.co.in>
2021-03-22 13:22:23 +0530

    agenda content added

--- spring-boot-app1/src/main/java/com/demo/spring/TestResource.java
+++ spring-boot-app1/src/main/java/com/demo/spring/TestResource.java
@@ -1,14 +1,20 @@
 package com.demo.spring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("classpath:messages.properties")
 public class TestResource {
 
+	@Value("${greet.message: no message Found}")
+	private String msg;
+	
 	@RequestMapping(path="/greet",method = RequestMethod.GET)
 	public String greet() {
-		return "Hi there, its working";
+		return msg;
 	}
 }
